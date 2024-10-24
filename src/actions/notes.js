04ddb26c83223335087dd2f131c7106132a9ab42
meshal <imeshalfm@gmail.com>
2024-10-24 11:21:47 +0300

    redirect to newly created note page

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,33 +1,37 @@
-'use server'
+'use server';
 
-import { revalidatePath } from "next/cache";
-import { notFound } from "next/navigation";
+import { revalidatePath } from 'next/cache';
+import { notFound } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
-import { baseUrl, getHeaders } from "./config";
+import { baseUrl, getHeaders } from './config';
 
 export async function fetchNotes() {
   const response = await fetch(`${baseUrl}/notes`);
-  const notes = await response.json()
-  return notes
+  const notes = await response.json();
+  return notes;
 }
 
 export async function fetchNoteById(noteId) {
   const response = await fetch(`${baseUrl}/notes/${noteId}`);
 
-  if (!response.ok) notFound()
+  if (!response.ok) notFound();
 
-  const note = await response.json()
-  return note
+  const note = await response.json();
+  return note;
 }
 
 export async function createNote(formData) {
-  const note = Object.fromEntries(formData)
+  const note = Object.fromEntries(formData);
 
   const response = await fetch(`${baseUrl}/notes`, {
-    method: "POST",
+    method: 'POST',
     headers: await getHeaders(),
-    body: JSON.stringify(note)
-  })
+    body: JSON.stringify(note),
+  });
 
-  revalidatePath('/notes')
+  const newNote = await response.json();
+  revalidatePath('/notes');
+  revalidatePath(`/notes/[noteId]`, 'page');
+  redirect(`/notes/${newNote._id}`);
 }
